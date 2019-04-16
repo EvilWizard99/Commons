f9e3ad5dd0725ef0de91229d8036ac5b6deba8d1
Evil Wizard <evil.wizard95@googlemail.com>
2019-04-16 10:28:16 +0100

    finished unit test for object

--- tests/ContextErrorTest.php
+++ tests/ContextErrorTest.php
@@ -10,22 +10,68 @@
 class ContextErrorTest extends PHPUnit_Framework_TestCase {
 	
 	/**
-	 * Just check if the ContextError has no syntax error 
+	 * @var	Exception Provides valid exception source to create ContextError from.
+	 */
+	protected $ex;
+	
+	/**
+	 * Setup the test scenario.
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->ex = new Exception("test error exception");
+	}
+	
+	/**
+	 * Provides valid source to create ContextError from.
+	 */
+	public function validConstructSourceProvider() {
+		return [
+			"new exception"	=> [$this->ex],
+			"no exception"	=> [NULL]
+		];
+	}
+	
+	/**
+	 * Provides invalid source to create ContextError from.
+	 */
+	public function invalidConstructSourceProvider() {
+		return [
+			"garbage"				=> ["bla"],
+			"invalid object source"	=> [new stdClass]
+		];
+	}
+	
+	/**
+	 * Just check if the ContextError has no syntax error.
 	 */
 	public function testIsThereAnySyntaxError() {
-		$var = new \EWC\Commons\Errors\ContextError(new Exception("test error exception"));
+		$var = new \EWC\Commons\Errors\ContextError();
 		$this->assertTrue(is_object($var));
 		unset($var);
 	}
   
 	/**
-	 * Just check if the ContextError returns the exception 
+	 * Just check if the ContextError returns the exception.
+	 * 
+	 * @dataProvider validConstructSourceProvider
 	 */
-	public function testGetException() {
-		$ex = new Exception("test error exception");
-		$var = new \EWC\Commons\Errors\ContextError($ex);
-		$this->assertTrue($var->getException() == $ex);
-		unset($var, $ex);
+	public function testGetExceptionReturnsAnException($data) {
+		$var = new \EWC\Commons\Errors\ContextError($data);
+		$this->assertInstanceOf("Exception", $var->getException());
+		unset($var);
+	}
+  
+	/**
+	 * Just check if the ContextError throws invalid construct.
+	 * 
+	 * @dataProvider invalidConstructSourceProvider
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	public function testThrowsInvalidConstructSource($data) {
+		$var = new \EWC\Commons\Errors\ContextError($data);
+		$this->assertInstanceOf("Exception", $var->getException());
+		unset($var);
 	}
   
 }
