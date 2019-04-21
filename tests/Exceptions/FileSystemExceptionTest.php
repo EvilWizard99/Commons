4746018cdab7492f48c8eb5525d5aba2437fadef
Evil Wizard <evil.wizard95@googlemail.com>
2019-04-21 12:54:53 +0100

    finished the tests for the exceptions

--- tests/Exceptions/FileSystemExceptionTest.php
+++ tests/Exceptions/FileSystemExceptionTest.php
@@ -15,12 +15,56 @@ use EWC\Commons\Exceptions\FileSystemException;
 class FileSystemExceptionTest extends PHPUnit_Framework_TestCase {
 	
 	/**
-	 * Just check if the ImmutableTraitException has no syntax error.
+	 * Just check if the FileSystemException has no syntax error.
 	 */
 	public function testIsThereAnySyntaxError() {
 		$var = new FileSystemException;
 		$this->assertTrue(is_object($var));
 		unset($var);
+	}
+	
+	/**
+	 * Make sure file not found throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\FileSystemException
+	 * @expectedExceptionMessageRegExp /File \[.+\] not found\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\FileSystemException::FILE_NOT_FOUND
+	 */
+	public function testFileNotFoundThrowsExceptionWithFileNotFound() {
+		throw FileSystemException::withFileNotFound("/file/not/found.ext");
+	}
+	
+	/**
+	 * Make sure make dir failed throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\FileSystemException
+	 * @expectedExceptionMessageRegExp /Failed to make directory \[.+\]\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\FileSystemException::MAKE_DIR_FAILED
+	 */
+	public function testMakeDirFailedThrowsExceptionWithMakeDirFailed() {
+		throw FileSystemException::withMakeDirFailed("/folder/to/fail");
+	}
+	
+	/**
+	 * Make sure open file for writing failed throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\FileSystemException
+	 * @expectedExceptionMessageRegExp /Unable to open \[.+\] for writing\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\FileSystemException::OPEN_FOR_WRITE
+	 */
+	public function testOpenFileForWritingThrowsExceptionWithWriteFileFailed() {
+		throw FileSystemException::withWriteFileFailed("/file/to/fail.ext");
+	}
+	
+	/**
+	 * Make sure write file contents failed throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\FileSystemException
+	 * @expectedExceptionMessageRegExp /Unable to write file contents for \[.+\]\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\FileSystemException::WRITE_FILE
+	 */
+	public function testWriteFileContentFailedThrowsExceptionWithWriteFileContentsFailed() {
+		throw FileSystemException::withWriteFileContentsFailed("/file/to/fail.ext");
 	}
   
 }
--- tests/Exceptions/ReflectorExceptionTest.php
+++ tests/Exceptions/ReflectorExceptionTest.php
@@ -4,6 +4,7 @@ namespace EWC\Commons\Tests\Exceptions;
 
 use PHPUnit_Framework_TestCase;
 use EWC\Commons\Exceptions\ReflectorException;
+use ReflectionException;
 
 /**
  * Corresponding Test Class for \EWC\Commons\Exceptions\ReflectorException
@@ -15,12 +16,80 @@ use EWC\Commons\Exceptions\ReflectorException;
 class ReflectorExceptionTest extends PHPUnit_Framework_TestCase {
 	
 	/**
-	 * Just check if the ImmutableTraitException has no syntax error.
+	 * @var	ReflectionException Provides valid exception to rethrow in the named exception methods.
+	 */
+	protected $ref_ex = NULL;
+	
+	/**
+	 * Setup the test fixture.
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->ref_ex = new ReflectionException("test reflection reflector exception", ReflectorException::REFLECTION_FAILED);
+	}
+	
+	/**
+	 * Just check if the ReflectorException has no syntax error.
 	 */
 	public function testIsThereAnySyntaxError() {
 		$var = new ReflectorException;
 		$this->assertTrue(is_object($var));
 		unset($var);
+	}
+	
+	/**
+	 * Make sure reflection failed throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\ReflectorException
+	 * @expectedExceptionMessageRegExp /Unable to get reflection of object \[.+\]\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\ReflectorException::REFLECTION_FAILED
+	 */
+	public function testReflectionFailedThrowsExceptionWithReflectionFailed() {
+		throw ReflectorException::withReflectionFailed("\\Namespaced\\Missing\\Classname", $this->ref_ex);
+	}
+	
+	/**
+	 * Make sure reflection instance failed throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\ReflectorException
+	 * @expectedExceptionMessageRegExp /Unable to get reflected instance of object \[.+\]\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\ReflectorException::REFLECTIED_INSTANCE_FAILED
+	 */
+	public function testReflectionInstancFailedeThrowsExceptionWithFailedToGetInstance() {
+		throw ReflectorException::withFailedToGetInstance("\\Namespaced\\Instance\\Fails\\Classname", $this->ref_ex);
+	}
+	
+	/**
+	 * Make sure calling a method out of scope throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\ReflectorException
+	 * @expectedExceptionMessageRegExp /Calling method \[.+\] directly is not permitted\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\ReflectorException::ILLEGAL_METHOD_CALL
+	 */
+	public function testCallingIllegalMethodThrowsExceptionWithIllegalMethodCall() {
+		throw ReflectorException::withIllegalMethodCall("out_of_scope_method");
+	}
+	
+	/**
+	 * Make sure reflected method argument count check failed throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\ReflectorException
+	 * @expectedExceptionMessageRegExp /.+::.+\(\) expects \d+ parameters, \d+ given\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\ReflectorException::ARGUMENT_COUNT_MISMATCH
+	 */
+	public function testArgumentCountMismatchThrowsExceptionWithArgumentCountMismatch() {
+		throw ReflectorException::withArgumentCountMismatch("\\Namespaced\\Classname", "method_name", 2, 1);
+	}
+	
+	/**
+	 * Make sure reflected class object type mismatch throws expected exception.
+	 * 
+	 * @expectedException \EWC\Commons\Exceptions\ReflectorException
+	 * @expectedExceptionMessageRegExp /\[.+\] does not match expected type \[.+\]\./
+	 * @expectedExceptionCode \EWC\Commons\Exceptions\ReflectorException::REFLECTION_TYPE_MISMATCH
+	 */
+	public function testReflectionClassTypeFailedThrowsExceptionWithTypeMismatch() {
+		throw ReflectorException::withTypeMismatch("\\Namespaced\\Classname", "\\Expected\\Namespaced\\Classname");
 	}
   
 }
